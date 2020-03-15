235fd9c6557e1d7052a226ee78e03d8099fa9f83
Freeze55 <Cherenkov.di@gmail.com>
2020-03-16 00:56:41 +0400

    add multithread execution

--- src/main/java/ru/freeze/Main.java
+++ src/main/java/ru/freeze/Main.java
@@ -9,7 +9,7 @@ public class Main{
 
         WatermarkVideoProcessor processor = new WatermarkVideoProcessor();
 
-        processor.run();
+        processor.multiThreadRun();
     }
 
 }
--- src/main/java/ru/freeze/watermark/WatermarkVideoProcessor.java
+++ src/main/java/ru/freeze/watermark/WatermarkVideoProcessor.java
@@ -1,10 +1,13 @@
 package ru.freeze.watermark;
 
 
-
-import org.bytedeco.javacv.FFmpegFrameGrabber;
-import org.bytedeco.javacv.FFmpegFrameRecorder;
+import org.bytedeco.javacv.*;
 import org.bytedeco.javacv.Frame;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.CountDownLatch;
 
 public class WatermarkVideoProcessor {
 
@@ -21,7 +24,7 @@ public class WatermarkVideoProcessor {
         frameGrabber.start();
 
 
-        FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(OUTPUT_FILE_PATH, frameGrabber.getImageWidth(), frameGrabber.getImageHeight(),frameGrabber.getAudioChannels());
+        FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(OUTPUT_FILE_PATH, frameGrabber.getImageWidth(), frameGrabber.getImageHeight(), frameGrabber.getAudioChannels());
         recorder.setFormat("mp4");
 
         recorder.setVideoCodec(frameGrabber.getVideoCodec());
@@ -48,13 +51,109 @@ public class WatermarkVideoProcessor {
         recorder.stop();
 
 
-
     }
 
 
+    public void multiThreadRun() throws FrameGrabber.Exception, InterruptedException, FrameRecorder.Exception {
 
-    public void multiThreadRun(){
+        final WatermarkOnImageProcessor watermarkOnImageProcessor = new WatermarkOnImageProcessor();
+        final FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(INPUT_FILE_PATH);
+        frameGrabber.setFormat("mp4");
 
+        Collection<Frame> frames = new ArrayList<>();
+        final Collection<Frame> framesT2 = new ArrayList<>();
+        final Collection<Frame> framesT3 = new ArrayList<>();
+        final CountDownLatch latch = new CountDownLatch(2);
+
+
+
+        frameGrabber.start();
+        int countFrames = frameGrabber.getLengthInFrames();
+        final int countFramesForThreads = (int) countFrames / 3;
+
+        Thread second = new Thread(new Runnable() {
+            public void run() {
+                FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(INPUT_FILE_PATH);
+                frameGrabber.setFormat("mp4");
+                try {
+                    frameGrabber.start();
+                    frameGrabber.setFrameNumber(countFramesForThreads);
+
+                    Frame frame = null;
+                    while (
+                            frameGrabber.getFrameNumber() < countFramesForThreads * 2 &&
+                                    (frame = frameGrabber.grab()) != null) {
+                        framesT2.add(watermarkOnImageProcessor.processImage(frame));
+                        System.out.println("frame " + frameGrabber.getFrameNumber());
+                    }
+                    frameGrabber.stop();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                latch.countDown();
+            }
+        });
+
+        Thread third = new Thread(new Runnable() {
+            public void run() {
+                FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(INPUT_FILE_PATH);
+                frameGrabber.setFormat("mp4");
+                try {
+                    /*frameGrabber.start();
+                    frameGrabber.setFrameNumber(countFramesForThreads * 2);
+
+                    Frame frame = null;
+                    while ((frame = frameGrabber.grab()) != null) {
+                        framesT3.add(watermarkOnImageProcessor.processImage(frame));
+                        System.out.println("frame " + frameGrabber.getFrameNumber());
+                    }
+                    frameGrabber.stop();
+                    */
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                latch.countDown();
+            }
+        });
+
+        second.start();
+        third.start();
+/*
+        frameGrabber.setFrameNumber(0);
+
+        Frame frame = null;
+        while (frameGrabber.getFrameNumber() < countFramesForThreads &&
+                (frame = frameGrabber.grab()) != null) {
+            frames.add(watermarkOnImageProcessor.processImage(frame));
+            System.out.println("frame " + frameGrabber.getFrameNumber());
+        }
+        */
+        frameGrabber.stop();
+
+        latch.await();
+
+        frames.addAll(framesT2);
+        frames.addAll(framesT3);
+
+        final FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(OUTPUT_FILE_PATH, frameGrabber.getImageWidth(), frameGrabber.getImageHeight(), frameGrabber.getAudioChannels());
+        recorder.setFormat("mp4");
+
+        recorder.setVideoCodec(frameGrabber.getVideoCodec());
+
+        recorder.setFrameRate(frameGrabber.getFrameRate());
+        recorder.setSampleFormat(frameGrabber.getSampleFormat());
+        recorder.setSampleRate(frameGrabber.getSampleRate());
+
+        recorder.start();
+
+        frames.forEach(f -> {
+            try {
+                recorder.record(f);
+            } catch (FrameRecorder.Exception e) {
+                e.printStackTrace();
+            }
+        });
+        recorder.stop();
 
     }
 
